08c0a7123c93f49fd68b7f0eb15e1457ed945d11
adam <adampalinkas@gmail.com>
2024-10-19 13:12:53 +0200

    Display consecutive days start day

--- lib/widget/profile/stats/consecutive_days.dart
+++ lib/widget/profile/stats/consecutive_days.dart
@@ -1,8 +1,10 @@
+import 'package:dhyana/model/consecutive_days.dart';
 import 'package:dhyana/model/profile.dart';
 import 'package:dhyana/util/profile_stats_report_updater.dart';
 import 'package:dhyana/widget/app_colors.dart';
 import 'package:dhyana/widget/app_theme_data.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ConsecutiveDaysDisplay extends StatefulWidget {
 
@@ -36,6 +38,7 @@ class _ConsecutiveDaysDisplayState extends State<ConsecutiveDaysDisplay> {
         Text('consecutive days',
           style: Theme.of(context).textTheme.bodyLarge
         ),
+        buildConsecutiveDaysStartTime(context),
       ],
     );
   }
@@ -55,6 +58,23 @@ class _ConsecutiveDaysDisplayState extends State<ConsecutiveDaysDisplay> {
         ),
       )
     );
+  }
+
+  Widget buildConsecutiveDaysStartTime(BuildContext context) {
+    final ConsecutiveDays consecutiveDays =
+      widget.profile.statsReport.consecutiveDays;
+    if (consecutiveDays.current > 0 && consecutiveDays.startedAt != null) {
+
+      Locale locale = Localizations.localeOf(context);
+      String formattedDate = DateFormat.yMMMMd(locale.toString()).format(consecutiveDays.startedAt!);
+
+      return Text('Since $formattedDate',
+        style: Theme.of(context).textTheme.bodySmall
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+
   }
 
 }
