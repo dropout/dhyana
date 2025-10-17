7221c95e468e7f30035b01cfc93639132ebf0ff4
adam <adampalinkas@gmail.com>
2025-10-17 17:25:30 +0200

    Remove unnecessary bloc provider

--- lib/widget/bloc_provider/all.dart
+++ lib/widget/bloc_provider/all.dart
@@ -1,5 +1,4 @@
 export 'presence_bloc_provider.dart';
-export 'profile_bloc_provider.dart';
 export 'sessions_bloc_provider.dart';
 export 'timer_bloc_providers.dart';
 export 'timer_settings_bloc_provider.dart';
