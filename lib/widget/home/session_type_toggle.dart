b2c534de6ade8b2fc9e3f36996f2cf9981ca2c80
adam <adampalinkas@gmail.com>
2026-04-21 15:52:35 +0200

    Refactor SessionTypeToggle to use StatefulWidget and implement mode change handling

--- lib/widget/home/session_type_toggle.dart
+++ lib/widget/home/session_type_toggle.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+
 import 'package:dhyana/enum/session_type.dart';
 import 'package:dhyana/widget/design_spec.dart';
 import 'package:dhyana/widget/util/app_context.dart';
+import 'package:flip_board/flip_widget.dart';
 import 'package:flutter/material.dart';
 
 /// Toggle button that switches the home screen between the timer settings view
 /// and the chanting settings view. The widget uses callback pattern to notify
 /// parent of mode changes.
-class SessionTypeToggle extends StatelessWidget {
+class SessionTypeToggle extends StatefulWidget {
+  final SessionType activeMode;
+  final ValueChanged<SessionType> onModeChanged;
+  final EdgeInsetsGeometry? padding;
+
   const SessionTypeToggle({
     required this.activeMode,
     required this.onModeChanged,
@@ -14,53 +21,86 @@ class SessionTypeToggle extends StatelessWidget {
     this.padding,
   });
 
-  final SessionType activeMode;
-  final ValueChanged<SessionType> onModeChanged;
-  final EdgeInsetsGeometry? padding;
+  @override
+  State<SessionTypeToggle> createState() => _SessionTypeToggleState();
+}
+
+class _SessionTypeToggleState extends State<SessionTypeToggle> {
+  late final StreamController<SessionType> _modeStreamController;
+
+  @override
+  void initState() {
+    super.initState();
+    _modeStreamController = StreamController<SessionType>();
+    _modeStreamController.add(widget.activeMode);
+  }
+
+  @override
+  void didUpdateWidget(covariant SessionTypeToggle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.activeMode != widget.activeMode) {
+      _modeStreamController.add(widget.activeMode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CustomInkWell(
+    return GestureDetector(
       onTap: () {
         context.hapticsTap();
-        SessionType newMode = (activeMode == SessionType.timer)
-          ? SessionType.chanting
-          : SessionType.timer;
-        onModeChanged(newMode);
+        SessionType newMode = (widget.activeMode == SessionType.timer)
+            ? SessionType.chanting
+            : SessionType.timer;
+        widget.onModeChanged(newMode);
       },
+      child: FlipWidget<SessionType>(
+        flipType: .spinFlip,
+        flipCurve: FlipWidget.bounceFastFlip,
+        itemStream: _modeStreamController.stream,
+        itemBuilder: _builder,
+        flipDirection: .left,
+        initialValue: (widget.activeMode == .timer) ? .chanting : .timer,
+      ),
+    );
+  }
+
+  Widget _builder(BuildContext context, SessionType? item) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.black,
+      ),
       child: Padding(
-        padding: padding ?? const EdgeInsets.all(DesignSpec.paddingMd),
+        padding: const EdgeInsets.all(DesignSpec.paddingMd),
         child: Icon(
-          (activeMode == SessionType.timer)
-            ? Icons.music_note
-            : Icons.timer_outlined,
+          (item == SessionType.timer) ? Icons.music_note : Icons.timer_outlined,
           color: Colors.white,
         ),
       ),
     );
   }
 
+  @override
+  void dispose() {
+    _modeStreamController.close();
+    super.dispose();
+  }
+
 }
 
 
 class CustomInkWell extends StatefulWidget {
-
   final Widget child;
   final VoidCallback? onTap;
 
-  const CustomInkWell({
-    required this.child,
-    this.onTap,
-    super.key,
-  });
+  const CustomInkWell({required this.child, this.onTap, super.key});
 
   @override
   State<CustomInkWell> createState() => _CustomInkWellState();
 }
 
 class _CustomInkWellState extends State<CustomInkWell>
-  with SingleTickerProviderStateMixin {
-
+    with SingleTickerProviderStateMixin {
   late final AnimationController _animationController;
   late final Animation<double> _progressAnimation;
 
@@ -73,10 +113,7 @@ class _CustomInkWellState extends State<CustomInkWell>
     );
 
     _progressAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeOut,
-      ),
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
     );
   }
 
@@ -94,35 +131,29 @@ class _CustomInkWellState extends State<CustomInkWell>
       children: [
         AnimatedBuilder(
           animation: _progressAnimation,
-          builder: (context, child) =>
-            CustomPaint(
-              painter: _InkRipplePainter(
-                endSize: 48,
-                progress: _progressAnimation.value,
-              ),
-              child: widget.child,
+          builder: (context, child) => CustomPaint(
+            painter: _InkRipplePainter(
+              endSize: 48,
+              progress: _progressAnimation.value,
             ),
+            child: widget.child,
+          ),
         ),
         GestureDetector(
           onTap: _onTap,
           behavior: HitTestBehavior.translucent,
           child: widget.child,
-        )
-      ]
+        ),
+      ],
     );
   }
-
 }
 
 class _InkRipplePainter extends CustomPainter {
-
   final double endSize;
   final double progress;
 
-  const _InkRipplePainter({
-    required this.endSize,
-    required this.progress,
-  });
+  const _InkRipplePainter({required this.endSize, required this.progress});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -144,14 +175,3 @@ class _InkRipplePainter extends CustomPainter {
     return true;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
